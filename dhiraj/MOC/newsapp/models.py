7de5aba924a8905459779096fcc7f07a6b8e31e6
thelostmonk <dj@localhost.localdomain>
2017-06-06 06:16:09 +0530

    created parser and edited models

--- dhiraj/MOC/newsapp/models.py
+++ dhiraj/MOC/newsapp/models.py
@@ -3,3 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+# will be using users class inbuilt in django
+
+class tbl_MST_NewsArticle(models.Model):
+	articleId = models.AutoField(primary_key=True)
+	author = models.CharField(max_length = 30)
+	title = models.CharField(max_length = 100)
+	description = models.CharField(max_length = 250)
+	url = models.CharField(max_length = 50)
+	urlToImage = models.CharField(max_length = 80)
+	publishedAt = models.DateField()
+
+	def __str__(self):
+		return(title)
--- dhiraj/MOC/newsparser/news_parser.py
+++ dhiraj/MOC/newsparser/news_parser.py
@@ -3,3 +3,19 @@ Author : Dhiraj Subramanian B
 This python module will be used for requesting NewsApi for fetching latest news. The latest news will be then updated to the application database.
 '''
 
+import requests, json
+
+class NewsParser(object):
+	# two methods, for initializing the json and pushing it to db
+	def parse():
+		jsonResult = requests.get("https://newsapi.org/v1/articles?source=the-verge&sortBy=top&apiKey=07dcf26d0ded41ba8436fb8bd233edba")
+		data = json.loads(jsonResult.text)
+		finalList = []
+		for article in data["articles"]:
+			temp = [article["author"], article["title"], article["description"], article["url"], article["urlToImage"], article["publishedAt"]]
+			finalList.append(temp)
+		pushToDatabase(finalList)
+		print(finalList)
+
+	def pushToDatabase(finalList):
+		pass
