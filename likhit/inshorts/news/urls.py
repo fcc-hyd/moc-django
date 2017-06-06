76485ed1b1ce0dcf9277502d4c42d5e402c82afa
Ravi RT Merugu <rrmerugu@gmail.com>
2017-06-06 12:30:33 +0530

    Revert "created base for the app"

--- likhit/inshorts/news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib.auth import views as auth_views
-from django.conf import settings
-from django.conf.urls.static import static
-from news import views
-
-urlpatterns = [
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
