b7cc1bf50728b7b629bdf2974b461d0e95cc82b9
root <root@localhost.localdomain>
2017-06-09 02:03:31 +0530

    Added RestAPI

--- hackrsource/MOC/settings.py
+++ hackrsource/MOC/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'newsapp',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- hackrsource/newsapp/admin.py
+++ hackrsource/newsapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(tbl_MST_NewsArticle)
+admin.site.register(tbl_TRN_NewsComment)
+
